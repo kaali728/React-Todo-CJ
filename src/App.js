51a5fecab0a97080f3af80dc4f1cfd7035d0efc1
Ali Karami <52680076+kaali728@users.noreply.github.com>
2021-03-17 12:05:21 +0100

    add newBoard

--- src/App.js
+++ src/App.js
@@ -1,14 +1,53 @@
 import "./styles.css";
+import { useState } from "react";
 import Board from "./components/Board";
 
 export default function App() {
+  const [boards, setBoards] = useState([
+    { boardName: "Todo" },
+    { boardName: "In progress" },
+    { boardName: "Completed" }
+  ]);
+  const [newBoardName, setNewBoardName] = useState("");
+  const [makeNewBoard, setMakeNewBoard] = useState(false);
+
+  function showNewBoard() {
+    setMakeNewBoard(!makeNewBoard);
+  }
+  function newBoard(e) {
+    e.preventDefault();
+    if (newBoardName.length > 0) {
+      setBoards((oldBoards) => [...oldBoards, { boardName: newBoardName }]);
+      setMakeNewBoard(false);
+    } else {
+      alert("You have to Enter a Board Name");
+    }
+  }
   return (
     <div className="App">
-      <span className="headerText">My Todo's 🍩</span>
+      <span className="headerText">
+        My Todo's{" "}
+        <span role="img" aria-labelledby="emojiDonut">
+          🍩
+        </span>
+      </span>
+      <div className="makeNewBoard" onClick={showNewBoard}>
+        {makeNewBoard ? "Cancel" : "New Board"}
+      </div>
+      {makeNewBoard ? (
+        <form onSubmit={newBoard}>
+          <input
+            className="formInput"
+            value={newBoardName}
+            onChange={(e) => setNewBoardName(e.target.value)}
+            placeholder="Board Name"
+          ></input>
+        </form>
+      ) : null}
       <div className="boardOverview">
-        <Board title={"Todo"} />
-        <Board title={"In progress"} />
-        <Board title={"Completed"} />
+        {boards.map((board, index) => (
+          <Board title={board.boardName} key={index} />
+        ))}
       </div>
     </div>
   );
--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,15 +1,17 @@
 import React from "react";
 import { MdDelete } from "react-icons/md";
+import { MdDragHandle } from "react-icons/md";
 
 export default function Todo({ task, setTasks, deleteTask, index }) {
   return (
     <div className="todoWrapper">
-      <div className="todotask">{task}</div> {/* <div>edit</div> */}
+      <div className="todotask">{task}</div>
       <MdDelete
         className="removeIcon"
         size="23"
         onClick={() => deleteTask(index)}
       />
+      <MdDragHandle className="removeIcon" size="23" />
     </div>
   );
 }
